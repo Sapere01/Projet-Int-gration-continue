7f16adbbf212b4c2d7fb5545f33a0094dc60eb88
B. Armel YAMEOGO <baowendsommearmel@gmail.com>
2024-04-16 18:28:20 +0000

    Ajout de modeles Ajout de todo.py

--- GestionTache/GestionTacheApp/models.py
+++ GestionTache/GestionTacheApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Utilisateur(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    typeCompte = models.CharField(max_length=200)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+class Tache(models.Model):
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True)
+
+    titre = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    etat = models.BooleanField(default=False)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
+
--- GestionTache/GestionTacheApp/views.py
+++ GestionTache/GestionTacheApp/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'hello_world.html', {})
 
 # Create your views here.
