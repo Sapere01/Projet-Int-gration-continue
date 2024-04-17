0525f7e57a81c487e4aba0912cdfd08f0a7f3364
B. Armel YAMEOGO <baowendsommearmel@gmail.com>
2024-04-17 13:53:59 +0000

    MOD Utilisateur model

--- GestionTache/GestionTacheApp/models.py
+++ GestionTache/GestionTacheApp/models.py
@@ -9,13 +9,6 @@ class Utilisateur(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def create_logged_user(self, username, type_compte):
-        """Creates a logged_user object with limited information."""
-        return {
-            'id': self.id,
-            'username': username,
-            'type_compte': type_compte,
-        }
 
 class SessionUtilisateur(models.Model):
     user = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
