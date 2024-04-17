75840fa7d9e1bb36aa25358f1cf5dc88c9a59111
B. Armel YAMEOGO <baowendsommearmel@gmail.com>
2024-04-17 13:41:12 +0000

    Utilisateur model

--- GestionTache/GestionTacheApp/models.py
+++ GestionTache/GestionTacheApp/models.py
@@ -1,18 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class Utilisateur(AbstractUser):
-    TYPES_COMPTE = (
-        ('standard', 'Standard'),
-        ('administrateur', 'Administrateur'),
-    )
+# Create your models here.
+class Utilisateur(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    typeCompte = models.CharField(max_length=200)
 
-    type_compte = models.CharField(max_length=20, choices=TYPES_COMPTE, default='standard')
-    email = models.EmailField(unique=True)
-
-
-    def __str__(self):
-        return self.username
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def create_logged_user(self, username, type_compte):
         """Creates a logged_user object with limited information."""
@@ -28,11 +23,15 @@ class SessionUtilisateur(models.Model):
     # Add other relevant fields for your session data
 
 class Tache(models.Model):
-    titre = models.CharField(max_length=100)
-    description = models.TextField()
-    terminee = models.BooleanField(default=False)
-    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+    utilisateur = models.ForeignKey(Utilisateur, on_delete=models.SET_NULL, null=True)
 
+    titre = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    etat = models.BooleanField(default=False)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titre
+
