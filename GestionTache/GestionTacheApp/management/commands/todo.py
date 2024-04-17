4321d6f534bd87edb58cc3daad1622dcd2a46e19
B. Armel YAMEOGO <baowendsommearmel@gmail.com>
2024-04-17 16:34:52 +0000

    Ajout de nouvelles fonctionnalités - modifier tache - supprimer tache

--- GestionTache/GestionTacheApp/management/commands/todo.py
+++ GestionTache/GestionTacheApp/management/commands/todo.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 from ...models import Tache, Utilisateur, SessionUtilisateur
+from django.db.models import Q
 
 class Command(BaseCommand):
     help = "Gestionnaire interactif de tâches"
@@ -8,6 +9,15 @@ class Command(BaseCommand):
     #     parser.add_argument('--m', type=str)
         # parser.add_argument('title', type=str)
         # parser.add_argument('description', type=str)
+
+    def beautify(self):
+        print("****************************************************************************")
+
+    def print_error(self, message):
+        self.stdout.write("\033[91m{}\033[0m".format(message))
+    
+    def print_success(self, message):
+        print("\033[92m{}\033[0m".format(message))
 
     def display_menu(self):
         print("Menu Principal")
@@ -20,8 +30,7 @@ class Command(BaseCommand):
         print("7. Quitter")
 
         choice = input("Entrez votre choix (1-7): ")
-
-        print("****************************************")
+        self.beautify()
 
         return choice
     
@@ -32,17 +41,18 @@ class Command(BaseCommand):
         # Authentication
         try:
             user = Utilisateur.objects.get(username=username)
-        except:
-            self.stdout.write("User does not exits")
+        except Exception:
+            self.print_error("User does not exits")
 
         if user.password == password :
             # Delete all existing sessions
             SessionUtilisateur.objects.all().delete()
             SessionUtilisateur.objects.create(user=user)
 
-            self.stdout.write(f"Successfully logged in as {username}")
+            self.print_success("Successfully logged in as "+username)
         else:
-            self.stdout.write("Invalid username or password")
+            self.print_error("Invalid username or password")
+            self.beautify()
 
     def logout(self):
         utilisateur = SessionUtilisateur.objects.all().first().user
@@ -56,7 +66,7 @@ class Command(BaseCommand):
         password = input("Entrez votre mot de passe: ")
 
         if Utilisateur.objects.filter(username=username).exists():
-            print(f"Erreur: Le nom d'utilisateur '{username}' existe déjà.")
+            self.print_error("Erreur: Le nom d'utilisateur "+username+" existe déjà.")
             return
 
         try:
@@ -64,9 +74,9 @@ class Command(BaseCommand):
             user.username = username
             user.password = password
             user.save()
-            print(f"Utilisateur créé: {username}")
-        except Exception as e:
-            print(f"Erreur lors de la création de l'utilisateur: {e}")
+            self.print_success("Utilisateur créé: "+username)
+        except Exception:
+            self.print_error("Erreur lors de la création de l'utilisateur")
 
     def create__task(self):
         try:
@@ -78,8 +88,75 @@ class Command(BaseCommand):
             Tache.objects.create(titre=title, description=description, utilisateur=utilisateur)
             self.stdout.write(f"Tâche créée: {title}")
         except Exception:
-            self.stdout.write("Aucune session active. Veuillez vous connecter d'abord.")
+            self.print_error("Aucune session active. Veuillez vous connecter d'abord...")
+            self.beautify()
 
+    def print__all_task(self):
+        try:
+            utilisateur = SessionUtilisateur.objects.all().first().user
+            try:
+                mes_taches = Tache.objects.filter(
+                    Q(utilisateur__username__icontains=utilisateur.username))
+                print("******************************** MES TACHES ********************************")
+                if mes_taches.count()==0:
+                    print("Aucune tâche disponible")
+                for tache in mes_taches:
+                    self.stdout.write(f"{tache.id} - {tache} : {tache.description}")
+                self.beautify()
+            except Exception:
+                self.print_error("Vous n'avez aucune tâche prévue...")
+                self.beautify()
+            
+        except Exception:
+            self.print_error("Aucune session active. Veuillez vous connecter d'abord...")
+            self.beautify()
+
+    def mod__task(self):
+        task_id = input("Entrez l'id de la tâche: ")
+
+        try:
+            utilisateur = SessionUtilisateur.objects.all().first().user
+
+            try:
+                task = Tache.objects.get(id=task_id)
+
+                new_titre = input("Entrez le nouveau libellé / Saisir 'no' pour ne pas modifier ce champ: ")
+                new_description = input("Entrez la nouvelle description / Saisir 'no' pour ne pas modifier ce champ: ")
+
+                if new_titre != "no":
+                    task.titre = new_titre
+
+                if new_description != "no":
+                    task.description = new_description
+
+                task.save()
+                self.print_success("Tache "+task_id+" modifié avec succès!")
+            except Exception:
+                self.print_error("Aucune tâche trouvée avec l'id "+task_id)
+                self.beautify()
+            
+        except Exception:
+            self.print_error("Aucune session active. Veuillez vous connecter d'abord...")
+            self.beautify()
+
+    def del__task(self):
+        task_id = input("Entrez l'id de la tâche: ")
+
+        try:
+            utilisateur = SessionUtilisateur.objects.all().first().user
+
+            try:
+                task_to_del = Tache.objects.get(id=task_id)
+                task_to_del.delete()
+
+                self.print_success("Tache "+task_id+" supprimée avec succès!")
+            except Exception:
+                self.print_error("Aucune tâche trouvée avec l'id "+task_id)
+                self.beautify()
+            
+        except Exception:
+            self.print_error("Aucune session active. Veuillez vous connecter d'abord...")
+            self.beautify()
 
     def handle(self, *args, **options):
 
@@ -94,16 +171,24 @@ class Command(BaseCommand):
                 case '2':
                     self.create__user()
                 case '3':
-                    self.create__task()
+                    self.print__all_task()
                 case '4':
                     self.create__task()
+                case '5':
+                    self.mod__task()
+                case '6':
+                    self.del__task()
                 case '7':
-                    # self.stdout.write(f"Tâche créée: {title}")
                     self.stdout.write("Aurevoir")
                     self.logout()
                     is_not_finish = False
+                case '9': # Supprimer toutes les tâches
+                    tasks = Tache.objects.all()
+                    for task in tasks:
+                        task.delete()
                 case _:
-                    self.stdout.write("Choix invalide. Veuillez réessayer.")
+                    self.print_error("Choix invalide. Veuillez réessayer.")
+
 
         
         
